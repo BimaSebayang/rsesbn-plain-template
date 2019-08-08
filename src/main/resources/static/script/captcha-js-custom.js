995ae0d26db9a3b5294925633170ba8b5a4b3b94
Roxas0309 <bimasebayang11@gmail.com>
2019-08-08 15:32:13 +0700

    commit baru

--- src/main/resources/static/script/captcha-js-custom.js
+++ src/main/resources/static/script/captcha-js-custom.js
@@ -6,8 +6,11 @@ var captcha;
 			var b = Math.floor((Math.random() * 10));
 			var c = Math.floor((Math.random() * 10));
 			var d = Math.floor((Math.random() * 10));
+			var e = Math.floor((Math.random() * 10));
+			var f = Math.floor((Math.random() * 10));
 
-			captcha = a.toString() + b.toString() + c.toString() + d.toString();
+			captcha = a.toString() + b.toString() + c.toString() + d.toString()
+			          +e.toString()+f.toString();
 
 			document.getElementById("output").innerHTML = captcha;
 			debugger;
